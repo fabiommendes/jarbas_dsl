84fdb82fadaa5e9cc201bc77b358708f967c0e88
Rodrigo <rodrigo.redcode@gmail.com>
2017-10-18 18:13:23 -0200

    Add more tests and change lexer tests file

--- src/jarbas_dsl/tests/test_jarbas_dsl.py
+++ src/jarbas_dsl/tests/test_jarbas_dsl.py
@@ -1,34 +1,7 @@
 import pytest
 import jarbas_dsl
-from jarbas_dsl.lexer import tokenize
 
 def test_project_defines_author_and_version():
     assert hasattr(jarbas_dsl, '__author__')
     assert hasattr(jarbas_dsl, '__version__')
 
-@pytest.fixture
-def test_lexer(string,expected):
-    tokenized_source = tokenize(string)
-    assert tokenized_source == expected
-
-def test_lexer_method_calling():
-    string = "Hello $name.title()!    // method calling"
-    expected = ["Hello ", "$name", ".title", "(", ")", "!    ", "// method calling"]
-    test_lexer(string, expected)
-
-def test_lexer_multiple_lines():
-    string = "Name: [name] //comment here\n Hello $name.title()!"
-    expected = ["Name: ", "[", "name","]", " ", "//comment here", "\n Hello ",
-                "$name", ".title", "(", ")", "!"]
-    test_lexer(string,expected)
-
-
-def test_lexer_if_calling():
-    string = "=if $condition"
-    expected = ["=", "if $condition"]
-    test_lexer(string,expected)
-
-def test_lexer_else_calling():
-    string = "=else \n"
-    expected = ["=", "else \n"]
-    test_lexer(string,expected)
---
+++ src/jarbas_dsl/tests/test_lexer.py
@@ -0,0 +1,85 @@
+import pytest
+import jarbas_dsl
+from jarbas_dsl.lexer import tokenize
+
+@pytest.fixture
+def test_lexer(string,expected):
+    tokenized_source = tokenize(string)
+    assert tokenized_source == expected
+
+
+# Tests of dollar sign small transformations and comments
+def test_lexer_attribute_access():
+    string = "Hello $person.name!"
+    expected = ["Hello ", "$person", ".name", "!"]
+    test_lexer(string, expected)
+
+def test_lexer_method_calling():
+    string = "Hello $name.title()!"
+    expected = ["Hello ", "$name", ".title", "(", ")", "!"]
+    test_lexer(string, expected)
+
+def test_pipe_operator():
+    string = "Hello $name|title!"
+    expected = ["Hello ", "$name", "|title", "!"]
+    test_lexer(string, expected)
+
+def test_comment():
+    string = "Hello $person.name! // attribute access"
+    expected = ["Hello ", "$person", ".name", "! ", "// attribute access"]
+    test_lexer(string, expected)
+
+
+# Tests of user input token
+def test_simple_user_input():
+    string = "Name: [name] Hello $name!"
+    expected = ["Name: ", "[", "name", "]", " Hello ", "$name", "!"]
+    test_lexer(string, expected)
+
+def test_defined_type_user_input():
+    string = "Name: [age=int] Hello $name!"
+    expected = ["Name: ", "[", "age", "=", "int", "]", " Hello ", "$name", "!"]
+    test_lexer(string, expected)
+
+def test_function_validation_user_input():
+    string = "Name: [email=&email] Hello $name!"
+    expected = ["Name: ", "[", "email", "=", "&", "email", "]", " Hello ", "$name", "!"]  
+    test_lexer(string, expected)
+
+def test_default_value_user_input():
+    string = "Name: [github=@email] Hello $name!"
+    expected = ["Name: ", "[", "github", "=", "@", "email", "]", " Hello ", "$name", "!"]  
+    test_lexer(string, expected)
+
+
+# Test of multiple lines source code
+def test_lexer_multiple_lines():
+    string = "Name: [name] //comment here\n Hello $name.title()!"
+    expected = ["Name: ", "[", "name","]", " ", "//comment here", "\n Hello ",
+                "$name", ".title", "(", ")", "!"]
+    test_lexer(string,expected)
+
+
+# Tests of conditional execution
+def test_lexer_if_conditional():
+    string = "=if $condition"
+    expected = ["=if ", "$condition"]
+    test_lexer(string,expected)
+
+def test_lexer_elif_conditional():
+    string = "=elif Do you want to proceed? [proceed=bool]"
+    expected = ["=elif ", "Do you want to proceed? ", "[", "proceed", "=", "bool", "]"]
+    test_lexer(string,expected)
+
+def test_lexer_else_conditional():
+    string = "=else \n"
+    expected = ["=", "else \n"]
+    test_lexer(string,expected)
+
+def test_lexer_all_conditionals():
+    string = "=if $is_minor(age)\nYou cannot proceed.\n=elif Do you want to proceed?\
+ [proceed=bool]\nOk, let's go!\n=else\nBye!\n=endif"
+    expected = ["=if ", "$is_minor", "(", "age", ")", "\nYou cannot proceed.\n", "=elif ",
+                "Do you want to proceed? ", "[", "proceed", "=", "bool", "]", "\nOk, let's go!\n",
+                "=else\n", "Bye!\n", "=endif"]
+    test_lexer(string,expected)
