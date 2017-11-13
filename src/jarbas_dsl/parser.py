597747180bb8c49265da0756b498244f085741d8
Rodrigo <rodrigo.redcode@gmail.com>
2017-11-13 11:02:45 -0200

    Create functions to minimize the use of lambda, add functions parser rules and change the attribute tuple structure

--- src/jarbas_dsl/parser.py
+++ src/jarbas_dsl/parser.py
@@ -5,7 +5,8 @@ from collections import namedtuple
 
 
 Var = namedtuple('Var', 'id')
-Attr = namedtuple('Attr', 'id')
+Func = namedtuple('Func', 'id args')
+Attr = namedtuple('Attr', 'id belong_to')
 Method = namedtuple('Method', 'id args')
 Filter = namedtuple('Filter', 'id')
 Expr = namedtuple('Expr', 'components')
@@ -25,6 +26,7 @@ def multi_arg(arg1, text, arg2):
         args = [arg1, arg2]
         return args
 
+# Input
 def normal_input(b_o, s, b_c):
     return Input(save_in=s, type=None, default=None, validate_func=None)
 
@@ -34,26 +36,56 @@ def type_input(b_o, s, eq, t, b_c):
 def default_input(b_o, s, eq, at, d, b_c):
     return Input(save_in=s, type=None, default=d, validate_func=None)
 
-def func_input(b_o, s, eq, ap, f, b_c):
+def validated_input(b_o, s, eq, ap, f, b_c):
     return Input(save_in=s, type=None, default=None, validate_func=f)
 
 
+# Variables
+def variable(id):
+    return Var(id.replace('$', ''))
+
+# Attributes
+def attribute(var, attr):
+    return Attr(attr.replace('.', ''), var.replace('$', ''))
+
+# Functions
+def no_args_func(id, p_o, p_c):
+    return Func(id.replace('$', ''), None)
+
+def args_func(id, p_o, args, p_c):
+    return Func(id.replace('$', ''), args)
+
+# Methods
+def no_args_method(id, p_o, p_c):
+    return Func(id.replace('.', ''), None)
+
+def args_method(id, p_o, args, p_c):
+    return Func(id.replace('.', ''), args)
+    
+# Filter
+def filter(id):
+    return Filter(id.replace('|', ''))
+
 parser_rules = [
     ('expr : expr expr', lambda x,y : Expr(components=[x, y])),
-    ('expr : access', lambda x: x),
-    ('access : ATTRIB PAREN_O PAREN_C', lambda m, p_o, p_c : Method(m.replace('.', ''), None)),
-    ('access : ATTRIB PAREN_O args PAREN_C', lambda m, p_o, a, p_c : Method(m.replace('.', ''), a)),
-    ('access : ATTRIB', lambda token : Attr(id=token.replace('.', ''))),
-    ('access : VARIABLE', lambda token : Var(id=token.replace('$', ''))),
-    ('access : PIPE_FILTER', lambda token : Filter(id=token.replace('|', ''))),
-    ('access : BRACKET_O TEXT BRACKET_C', normal_input),
-    ('access : BRACKET_O TEXT EQUAL TEXT BRACKET_C', type_input),
-    ('access : BRACKET_O TEXT EQUAL AT TEXT BRACKET_C', default_input),
-    ('access : BRACKET_O TEXT EQUAL AMPER TEXT BRACKET_C', func_input),
+    ('expr : utility', lambda x: x),
+    ('utility : ATTRIB PAREN_O PAREN_C', no_args_method),
+    ('utility : ATTRIB PAREN_O args PAREN_C', args_method),
+    ('utility : VARIABLE PAREN_O PAREN_C', no_args_func),
+    ('utility : VARIABLE PAREN_O args PAREN_C', args_func),
+    ('utility : VARIABLE ATTRIB', attribute),
+    ('utility : VARIABLE', variable),
+    ('utility : PIPE_FILTER', filter),
+    ('utility : BRACKET_O TEXT BRACKET_C', normal_input),
+    ('utility : BRACKET_O TEXT EQUAL TEXT BRACKET_C', type_input),
+    ('utility : BRACKET_O TEXT EQUAL AT TEXT BRACKET_C', default_input),
+    ('utility : BRACKET_O TEXT EQUAL AMPER TEXT BRACKET_C', validated_input),
     ('args : arg TEXT args', multi_arg),
     ('args : arg', lambda token : token),
     ('expr : text', lambda x: x),
     ('expr : comment', lambda x: x),
+    ('arg : VARIABLE', variable),
+    ('arg : VARIABLE ATTRIB', attribute),
     ('arg : NUMBER', lambda token : Num(value=token)),
     ('arg : BOOLEAN', lambda token : Bool(value=token)),
     ('arg : STRING', lambda token : Str(value=token.replace("'", ''))),
@@ -72,7 +104,7 @@ class Parser:
     def __init__(self, source):
         self.source = source
         self.tokens = tokenize(source)
-        #print(self.tokens)
+        print(self.tokens)
 
     def parse(self):
         """
@@ -80,8 +112,8 @@ class Parser:
         """
         return parser(self.tokens)
 
-#s = "Hello $person|title! //alou"
-s = "Name: [name] // string input"
+s = "$is_minor($age.name)"
+#s = "=if $is_minor($age)"
 p = Parser(s)
 #a = p.test()
 #print(a)
