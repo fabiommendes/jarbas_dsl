97773d29f362495ba5bde628f06fb9a2d77b3f03
pablodiegoss <pablodiegoss@hotmail.com>
2017-11-17 11:51:43 -0200

    Reorganize tests for Parser

--- src/jarbas_dsl/tests/test_parser.py
+++ src/jarbas_dsl/tests/test_parser.py
@@ -8,11 +8,11 @@ def test_parser(test_string):
     return str(Parser(test_string).parse())
 
 
-def test_method():
-    string = "$m.is_minor($person.age, True)"
-    expected = ("Method(id='is_minor', belongs_to=Var(id='m'), args=[Attr(id='age', belongs_to=Var(id='person')), Bool(value='True')])")
-
-    assert test_parser(string) == expected
+"""
+=================================
+Tests for Inputs
+=================================
+"""
 
 
 def test_input_without_default():
@@ -43,30 +43,76 @@ def test_input_with_type():
     assert test_parser(string) == expected
 
 
+"""
+=================================
+Tests for Texts and Strings
+=================================
+"""
+
+
 def test_string_without_function():
     string = "'振深面作果品選学我岐縦'"
     try:
         test_parser(string)
+        assert False
     except(SyntaxError):
         assert True
 
 
 def test_chinese_text():
-    string = "振深面作果品選学我岐縦判裁見。出位改購覇野告公料愛芸生多提係属。経最康覧政料区総栗様気投無種警込。中介注社保道験爆夏保逃聞気畑心全名買部。滑広光階芸座発態文指議球立度驚"
+    string = ("振深面作果品選学我岐縦判裁見。出位改購覇野告公料愛芸生多提係属。" +
+              "経最康覧政料区総栗様気投無種警込。中介注社保道験爆夏保逃聞気畑心" +
+              "全名買部。滑広光階芸座発態文指議球立度")
 
-    expected = "Text(value='振深面作果品選学我岐縦判裁見。出位改購覇野告公料愛芸生多提係属。経最康覧政料区総栗様気投無種警込。中介注社保道験爆夏保逃聞気畑心全名買部。滑広光階芸座発態文指議球立度驚')"
+    expected = ("Text(value='振深面作果品選学我岐縦判裁見。出位改購覇野告公料" +
+                "愛芸生多提係属。経最康覧政料区総栗様気投無種警込。中介注社保道" +
+                "験爆夏保逃聞気畑心全名買部。滑広光階芸座発態文指議球立度')")
     assert test_parser(string) == expected
 
 
-def test_string_with_function():
-    string = "振深面作果品選学我$function('出位改購覇野提係属。経最康様気投無種警込。')広光階芸座発議球立度驚"
-
-    expected = "Expr(components=[Text(value='振深面作果品選学我'), Expr(components=[Func(id='function', args=[Str(value='出位改購覇野提係属。経最康様気投無種警込。')]), Text(value='広光階芸座発議球立度驚')])])"
-    assert test_parser(string) == expected
+"""
+=================================
+Tests for Functions and Methods parsing
+=================================
+"""
 
 
 def test_validate_func():
     string = "$validate_user($username)"
 
     expected = "Func(id='validate_user', args=[Var(id='username')])"
+    assert test_parser(string) == expected
+
+def test_validate_func_without_param():
+    string = "$validate_user()"
+    # Question: should this work?
+    expected = "Func(id='validate_user', args=None)"
+    assert test_parser(string) == expected
+
+
+def test_function_with_strings():
+    string = "振深面作果品選学我$function('出位改購覇野提係属。経最康様気投無種警込。')広光階芸座発議球立度驚"
+
+    expected = "Expr(components=[Text(value='振深面作果品選学我'), Expr(components=[Func(id='function', args=[Str(value='出位改購覇野提係属。経最康様気投無種警込。')]), Text(value='広光階芸座発議球立度驚')])])"
+    assert test_parser(string) == expected
+
+
+def test_method_with_no_args():
+    string = "$m.is_minor()"
+    expected = ("Method(id='is_minor', belongs_to=Var(id='m'), args=None)")
+
+    assert test_parser(string) == expected
+
+
+def test_method_with_one_arg():
+    string = "$m.is_minor($person.age)"
+    expected = ("Method(id='is_minor', belongs_to=Var(id='m'), args=[Attr(id='age', belongs_to=Var(id='person'))])")
+
+    assert test_parser(string) == expected
+
+
+def test_method_with_two_args():
+    string = "$m.is_minor($person.age, True)"
+    expected = ("Method(id='is_minor', belongs_to=Var(id='m'), args=[Attr(id='age', belongs_to=Var(id='person')), Bool(value='True')])")
+
     assert test_parser(string) == expected
